274d24049b470f8746b992bcf5ec1529a5ba1c55
AbhinandAmmanamkandy <abhinandammanamkandypvt@gmail.com>
2025-03-14 13:11:01 +0530

    Download From GitHub

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("Redmi Note 13 Pro 5G")
+import requests
+import urllib.request
+from bs4 import BeautifulSoup
+
+base_url = "https://github.com"
+sub_url = "tiann/KernelSU"
+url = f"{base_url}/{sub_url}/releases/latest"
+
+response = requests.get(url, allow_redirects=True)
+soup = BeautifulSoup(response.text, "html.parser")
+
+version = "v" + soup.title.string.split("v", 1)[-1].split()[0]
+
+url = f"{base_url}/{sub_url}/releases/expanded_assets/{version}"
+response = requests.get(url, allow_redirects=True)
+
+soup = BeautifulSoup(response.text, "html.parser")
+apk_links = [f"{base_url}{a.get("href")}" for a in soup.find_all("a", href=True) if a["href"].endswith(".apk")]
+apks = [link.split('/')[-1] for link in apk_links]
+
+for i, name in enumerate(apks, 1):
+    print(f"{i}. {name}")
+
+choice = int(input("Enter choice: ")) - 1
+urllib.request.urlretrieve(apk_links[choice], apks[choice])
